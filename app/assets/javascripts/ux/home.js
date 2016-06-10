1dec2b3bbc9f29b958a7a635eca5c9a13cd960eb
karthik selvakumar <karthikselvakumar7@gmail.com>
2016-06-10 11:13:29 -0700

    Added sample json

--- app/assets/javascripts/ux/home.js
+++ app/assets/javascripts/ux/home.js
@@ -45,9 +45,91 @@ UX.RESULTS = (function() {
 		displayErrors($('#json-input'));
 	};
 
+
+	var loadSample = function() {
+		var jsonData = {
+		  "links": {
+		    "self": "http://example.com/articles",
+		    "next": "http://example.com/articles?page[offset]=2",
+		    "last": "http://example.com/articles?page[offset]=10"
+		  },
+		  "data": [{
+		    "type": "articles",
+		    "id": "1",
+		    "attributes": {
+		      "title": "JSON API paints my bikeshed!"
+		    },
+		    "relationships": {
+		      "author": {
+		        "links": {
+		          "self": "http://example.com/articles/1/relationships/author",
+		          "related": "http://example.com/articles/1/author"
+		        },
+		        "data": { "type": "people", "id": "9" }
+		      },
+		      "comments": {
+		        "links": {
+		          "self": "http://example.com/articles/1/relationships/comments",
+		          "related": "http://example.com/articles/1/comments"
+		        },
+		        "data": [
+		          { "type": "comments", "id": "5" },
+		          { "type": "comments", "id": "12" }
+		        ]
+		      }
+		    },
+		    "links": {
+		      "self": "http://example.com/articles/1"
+		    }
+		  }],
+		  "included": [{
+		    "type": "people",
+		    "id": "9",
+		    "attributes": {
+		      "first-name": "Dan",
+		      "last-name": "Gebhardt",
+		      "twitter": "dgeb"
+		    },
+		    "links": {
+		      "self": "http://example.com/people/9"
+		    }
+		  }, {
+		    "type": "comments",
+		    "id": "5",
+		    "attributes": {
+		      "body": "First!"
+		    },
+		    "relationships": {
+		      "author": {
+		        "data": { "type": "people", "id": "2" }
+		      }
+		    },
+		    "links": {
+		      "self": "http://example.com/comments/5"
+		    }
+		  }, {
+		    "type": "comments",
+		    "id": "12",
+		    "attributes": {
+		      "body": "I like XML better"
+		    },
+		    "relationships": {
+		      "author": {
+		        "data": { "type": "people", "id": "9" }
+		      }
+		    },
+		    "links": {
+		      "self": "http://example.com/comments/12"
+		    }
+		  }]
+		};
+		$('#json-input').html(JSON.stringify(jsonData, null, 2));
+	};
+
 	return {
 		displayErrors: displayErrors,
-		runCheck: runCheck
+		runCheck: runCheck,
+		loadSample: loadSample
 	};
 })();
 
@@ -59,6 +141,4 @@ $( document ).ready(function() {
 	    height: '700px',
       railVisible: true
 	});
-
-	
 });
